f07a4207a8d948001d9d7c3f86eb77f7170ef4c9
Cameron <cameronratliff93@gmail.com>
2025-09-25 09:06:24 -0400

    background js

--- background.js
+++ background.js
@@ -1,3 +1,14 @@
 chrome.runtime.onInstalled.addListener(() => {
-  console.log("Fantasy Assistant installed!");
+  console.log('Fantasy Assistant installed!');
+});
+
+// Listen for tab updates to detect when user navigates to Yahoo Fantasy Football
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  // Only proceed if the tab is complete and has a URL
+  if (changeInfo.status === 'complete' && tab.url) {
+    // Check if the URL starts with the Yahoo Fantasy Football domain
+    if (tab.url.startsWith('https://football.fantasysports.yahoo.com/')) {
+      console.log('User is on Yahoo Fantasy Football:', tab.url);
+    }
+  }
 });
