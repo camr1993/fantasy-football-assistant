2e3f76b44ec59ca04db702af9a4636ed28b43138
Cameron <cameronratliff93@gmail.com>
2025-10-23 10:03:31 -0400

    supabase fix

--- supabase/functions/utils/supabase.ts
+++ supabase/functions/utils/supabase.ts
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from 'npm:@supabase/supabase-js@^2.76.1';
 
 const supabaseUrl = Deno.env.get('SUPABASE_URL')!;
 const supabaseServiceKey = Deno.env.get('SUPABASE_SERVICE_ROLE_KEY')!;
